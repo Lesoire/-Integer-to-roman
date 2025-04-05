2393fff1130b2f510e23821189c8be4aebd2f10e
unknown <ivanitskyi.maksym@chnu.edu.ua>
2025-04-05 23:42:09 +0300

    Integer to roman

--- src/main/java/edu/pzks/Main.java
+++ src/main/java/edu/pzks/Main.java
@@ -3,7 +3,7 @@ package edu.pzks;
 public class Main {
 
     public static String intToRoman(int num) {
-        // Масиви римських чисел і відповідних їм значень
+
         int[] values =    {1000, 900, 500, 400, 100, 90,  50, 40,  10,  9,   5,  4,   1};
         String[] romans = {"M",  "CM","D", "CD","C", "XC","L","XL","X", "IX","V","IV","I"};
 
